0ae147d8310e6f3b4fbccaf29fd1db48ca4d348c
Andriy Semenets <semanser@gmail.com>
2019-02-21 17:22:12 +0200

    Add .make.sh to create all symlinks

---
+++ .make.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# .make.sh 
+# This script creates symlinks from ~/ to dotfiles dir
+# Before running this file, don't forget to run
+# cmod +x .make.sh
+
+dir=~/dotfiles
+olddir=~/dotfiles_old
+files=".bashrc .vimrc .zshrc .gitconfig .tmux.conf"
+
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...complete."
+
+echo "Changing to the $dir directory"
+cd $dir
+echo "...complete."
+
+for file in $files; do
+  echo "Moving existing dotfiles from ~ to $olddir"
+  mv ~/$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/$file
+done
