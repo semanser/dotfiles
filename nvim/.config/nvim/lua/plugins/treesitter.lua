642e0b4aa7f37156cd5895c3e12fc979129cbaf2
Andriy Semenets <semanser@gmail.com>
2024-01-14 01:09:48 +0100

    Improve treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,20 +1,40 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-		ignore_install = {}, -- List of parsers to ignore installing
-		highlight = {
-			enable = true, -- false will disable the whole extension
-			disable = {}, -- list of language that will be disabled
-			-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-			-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-			-- Using this option may slow down your editor, and you may see some duplicate highlights.
-			-- Instead of true it can also be a list of languages
-			-- additional_vim_regex_highlighting = true,
-		},
-		indent = {
-			enable = false,
-		},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "VeryLazy",
+		build = ":TSUpdate",
+		config = function()
+			local configs = require("nvim-treesitter.configs")
+
+			configs.setup({
+				ensure_installed = {
+					"lua",
+					"sql",
+					"javascript",
+					"go",
+					"xml",
+					"json",
+					"typescript",
+					"tsx",
+					"html",
+					"css",
+					"yaml",
+					"toml",
+					"rust",
+					"bash",
+					"python",
+					"dockerfile",
+					"vue",
+					"svelte",
+					"php",
+					"java",
+					"regex",
+					"query",
+				},
+				sync_install = false,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
 	},
 }
