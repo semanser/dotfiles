58f0d414be6cf4909d2849f6d353dd676cf25253
Andriy Semenets <semanser@gmail.com>
2024-09-13 23:01:34 +0200

    Make the nvim-tree as a modal

--- nvim/.config/nvim/lua/plugins/etc.lua
+++ nvim/.config/nvim/lua/plugins/etc.lua
@@ -35,7 +35,55 @@ return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		event = "VeryLazy",
-		opts = {},
+		opts = {
+			view = {
+				width = 35,
+			},
+			actions = {
+				open_file = {
+					quit_on_open = true,
+				},
+			},
+		},
+		config = function()
+			local api = require("nvim-tree.api")
+
+			local HEIGHT_RATIO = 0.8 -- You can change this
+			local WIDTH_RATIO = 0.5 -- You can change this too
+
+			require("nvim-tree").setup({
+				view = {
+					float = {
+						enable = true,
+						open_win_config = function()
+							local screen_w = vim.opt.columns:get()
+							local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+							local window_w = screen_w * WIDTH_RATIO
+							local window_h = screen_h * HEIGHT_RATIO
+							local window_w_int = math.floor(window_w)
+							local window_h_int = math.floor(window_h)
+							local center_x = (screen_w - window_w) / 2
+							local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+							return {
+								border = "rounded",
+								relative = "editor",
+								row = center_y,
+								col = center_x,
+								width = window_w_int,
+								height = window_h_int,
+							}
+						end,
+					},
+					width = function()
+						return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+					end,
+				},
+			})
+
+			api.events.subscribe(api.events.Event.FileCreated, function(file)
+				vim.cmd("vsplit " .. file.fname)
+			end)
+		end,
 	},
 	{
 		"folke/flash.nvim",
@@ -50,5 +98,14 @@ return {
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
+	},
+	{
+		"roberte777/keep-it-secret.nvim",
+		config = function()
+			require("keep-it-secret").setup({
+				wildcards = { ".*(.env)$", ".*(.secret)$" },
+				enabled = true,
+			})
+		end,
 	},
 }
