8d87ab19c943940a53fc7a62fa319d633be4fc9d
Andriy Semenets <semanser@gmail.com>
2024-09-13 23:00:59 +0200

    Run the formatter

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -1,23 +1,25 @@
 return {
-	"hoob3rt/lualine.nvim",
-	opts = {
-		options = {
-			theme = "catppuccin",
-			icons_enabled = true,
-			globalstatus = false,
-		},
-		sections = {
-			lualine_a = { "" },
-			lualine_b = { "diagnostics" },
-			lualine_c = {
-				{
-					"filename",
-					path = 0,
-				},
+	{
+		"hoob3rt/lualine.nvim",
+		opts = {
+			options = {
+				theme = "catppuccin",
+				icons_enabled = true,
+				globalstatus = false,
 			},
-			lualine_x = { "" },
-			lualine_y = { "searchcount" },
-			lualine_z = { "location" },
+			sections = {
+				lualine_a = { "" },
+				lualine_b = { "diagnostics" },
+				lualine_c = {
+					{
+						"filename",
+						path = 0,
+					},
+				},
+				lualine_x = { "copilot" },
+				lualine_y = { "searchcount" },
+				lualine_z = { "location" },
+			},
 		},
 	},
 }
