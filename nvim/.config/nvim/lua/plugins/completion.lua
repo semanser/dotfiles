c77db98d6557787d5e017ba49b5c73e2dff86075
Andriy Semenets <semanser@gmail.com>
2024-09-13 23:00:23 +0200

    Small lsp tweak

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -60,9 +60,11 @@ return {
 				formatting = {
 					format = lspkind.cmp_format({
 						mode = "symbol_text", -- show only symbol annotations
-						maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+						-- maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
 						-- can also be a function to dynamically calculate max width such as
-						-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+						maxwidth = function()
+							return math.floor(0.5 * vim.o.columns)
+						end,
 						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 						symbol_map = { Copilot = "" },
