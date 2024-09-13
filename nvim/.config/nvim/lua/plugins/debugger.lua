dde53d5ff2799cd706ab8bafafa4f3ac30fbc18b
Andriy Semenets <semanser@gmail.com>
2024-09-13 22:59:47 +0200

    Add nvim-dap integration

---
+++ nvim/.config/nvim/lua/plugins/debugger.lua
@@ -0,0 +1,33 @@
+return {
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			require("dapui").setup()
+
+			local dap, dapui = require("dap"), require("dapui")
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"leoluz/nvim-dap-go",
+		config = function()
+			require("dap-go").setup()
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		event = "BufRead",
+	},
+}
