0f8bdfcdebefa6344ac819558a6d404a176eda89
Andriy Semenets <semanser@gmail.com>
2025-06-25 13:02:40 +0200

    Use neogit

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		event = "VeryLazy",
 		opts = {},
 	},
 	{
@@ -9,4 +8,22 @@ return {
 		event = "VeryLazy",
 		dependencies = { "nvim-lua/plenary.nvim" },
 	},
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+      "ibhagwan/fzf-lua",              -- optional
+      "echasnovski/mini.pick",         -- optional
+      "folke/snacks.nvim",             -- optional
+    },
+    config = function()
+      require("neogit").setup({
+        kind = "floating"
+      })
+    end,
+  }
 }
